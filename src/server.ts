f5a31ad39525186e37d6c27dbc1fadd976511d67
Victor Alves <victoralves@fortics.com.br>
2022-06-23 05:24:59 -0300

    chore: config vscode debug

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => res.send('Olá'))
+
+app.post('/course', (req, res) => {
+  const { name } = req.body
+
+  return res.json({ name })
+})
 
 app.listen(3333, () => console.log('Server running!'))
