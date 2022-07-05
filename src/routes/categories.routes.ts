3a23d48b4d5f5e8ce781da45dfaa8564623555ce
Victor Alves <victoralves@fortics.com.br>
2022-07-05 12:16:55 -0300

    refactor: change specifications architecture

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,12 @@
 import { Router } from 'express'
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService'
+
+import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
-categoriesRoutes.post('/', (req, res) => {
-  const { name, description } = req.body
+categoriesRoutes.post('/', (req, res) => createCategoryController.handle(req, res))
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository)
-
-  createCategoryService.execute({ name, description })
-
-  return res.status(201).send()
-})
-
-categoriesRoutes.get('/', (req, res) => {
-  const categories = categoriesRepository.list()
-
-  return res.json(categories)
-})
+categoriesRoutes.get('/', (req, res) => listCategoriesController.handle(req, res))
 
 export { categoriesRoutes }
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,24 +1,12 @@
 import { Router } from 'express'
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository'
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService'
+
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification'
+import { listSpecificationsController } from '../modules/cars/useCases/listSpecifications'
 
 const specificationsRoutes = Router()
-const specificationsRepository = new SpecificationsRepository()
 
-specificationsRoutes.post('/', (req, res) => {
-  const { name, description } = req.body
+specificationsRoutes.post('/', (req, res) => createSpecificationController.handle(req, res))
 
-  const createSpecificationService = new CreateSpecificationService(specificationsRepository)
-
-  createSpecificationService.execute({ name, description })
-
-  return res.status(201).send()
-})
-
-specificationsRoutes.get('/', (req, res) => {
-  const specifications = specificationsRepository.list()
-
-  return res.json(specifications)
-})
+specificationsRoutes.get('/', (req, res) => listSpecificationsController.handle(req, res))
 
 export { specificationsRoutes }
