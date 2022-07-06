89a11ce011ba5a4b6c57bc4251e0c10047246ba1
Victor Alves <victoralves@fortics.com.br>
2022-07-06 14:52:14 -0300

    feat: add route to import categories

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,7 +1,45 @@
+import { parse } from 'csv-parse'
+import fs from 'fs'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+
+interface IImportCategory {
+  name: string
+  description: string
+}
 
 class ImportCategoryUseCase {
-  execute (file: Express.Multer.File): void {
-    console.log(file)
+  constructor (private categoriesRepository: ICategoriesRepository) {}
+
+  async loadCategories (file: Express.Multer.File): Promise<IImportCategory[]> {
+    return await new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
+
+      const parseFile = parse()
+      stream.pipe(parseFile)
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line
+          categories.push({ name, description })
+        })
+        .on('end', () => resolve(categories))
+        .on('error', (err) => reject(err))
+    })
+  }
+
+  async execute (file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+
+    categories.forEach(async (category) => {
+      const { name, description } = category
+
+      const categoryAlreadyExists = this.categoriesRepository.findByName(name)
+
+      if (!categoryAlreadyExists) {
+        this.categoriesRepository.create({ name, description })
+      }
+    })
   }
 }
 
